bca020973a4fec30d45fa2188266db04a8401c38
Marjory <samaterra@hotmail.com>
2022-05-29 16:00:52 -0400

    updating dependencies 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgChartsModule } from 'ng2-charts';
-import { NavComponent } from './nav/nav.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashComponent } from './dash/dash.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    DashComponent
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -35,7 +38,9 @@ import { MatMenuModule } from '@angular/material/menu';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+/*!
+==============================================================
+* Admin Dashboard Angular Material -
+==============================================================
+
+* Product Page: https://github.com/Marjory00/Admin-dashboard.git
+* Copyright 2022 Marjory Marquez
+* Licensed under MIT (https://github.com/Marjory00/Admin-dashboard/blob/master/LICENSE.md)
+
+* Coded by Marjory Marquez
+==============================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'hammerjs';
+
 
 if (environment.production) {
   enableProdMode();
