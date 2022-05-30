bad0df73125a90cd651f88d81bd4722c16150678
Marjory <samaterra@hotmail.com>
2022-05-30 05:13:07 -0400

    navbar.component.ts adding

--- src/app/dashboard/components/components/navbar/navbar.component.ts
+++ src/app/dashboard/components/components/navbar/navbar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from './../../sidebar/sidebar.component';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+import { ROUTES } from '../sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+private listTitles: any[];
+  location: Location;
+  mobile_menu_visible: any = 0;
+private toggleButton: any;
+private sidebarVisible: boolean;
 
-  constructor() { }
+
+  constructor(location: Location, private element: ElementRef, private router: Router) {
+    this.location = location;
+      this.sidebarVisible = false;
+  }
 
   ngOnInit() {
+    this.listTitles =
   }
 
 }
