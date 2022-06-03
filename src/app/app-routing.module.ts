30302a55fe47c9ee0e06b1852aef41d97c9b0b4b
Marjory <samaterra@hotmail.com>
2022-06-02 23:55:31 -0400

    admin-0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './home/home.component';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,36 +6,42 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
-   { path: '',
-   redirectTo: 'dashboard',
-   pathMatch: 'full',
-},
-{
-path: '',
-component: AdminLayoutComponent,
-children: [{
-  path: '',
-  loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
-}]
-}
+
+
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: '', component: DashboardComponent, pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', component: AdminLayoutComponent, pathMatch: 'full' },
+    { path: '', component: FooterComponent, pathMatch: 'full' },
+    { path: '', component: NavbarComponent, pathMatch: 'full' },
+    { path: '', component: SidebarComponent, pathMatch: 'full' },
+    { path: '', component: MapsComponent, pathMatch: 'full' },
+    { path: '', component: NotificationsComponent, pathMatch: 'full' },
+    { path: '', component: TableListComponent, pathMatch: 'full' },
+    { path: '', component: UserProfileComponent, pathMatch: 'full' },
+
 ];
 
+
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes,
-      {
-        useHash: true // angular static Base URL and routing with useHash:true
-      })
-
-  ],
-  exports: [
-
-  ],
+imports: [
+  CommonModule,
+  BrowserModule,
+  RouterModule.forRoot(routes,{
+     useHash: true
+  })
+],
+exports: [
+],
 })
-
 export class AppRoutingModule { }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
 
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 
 
 @NgModule({
@@ -18,9 +18,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     SidebarComponent
   ],
   exports: [
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent
+
   ]
 })
 export class ComponentsModule { }
