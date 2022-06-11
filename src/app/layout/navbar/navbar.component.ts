76bad75ba70a5d0c888430adb72bc9a1183a8c12
Marjory <samaterra@hotmail.com>
2022-06-11 00:54:15 -0400

    angular-material-module

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
